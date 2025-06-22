5d34122354fc49a90bfd497456f99fab286ec5ca
manarraafatt <manarraafat882@gmail.com>
2025-06-22 08:32:18 +0300

    >

--- lib/Feature/Home/Presentation/views/Widgets/HomeViewBody.dart
+++ lib/Feature/Home/Presentation/views/Widgets/HomeViewBody.dart
@@ -113,7 +113,8 @@ class HomeViewBody extends StatelessWidget {
                         id: 1,
                         body:
                             '${S.of(context).incompleteTrainingHours} ${getUserAcademicInfo()!.gainedHours}',
-                        title: S.of(context).admin,
+                        // title: S.of(context).admin,
+                        title: "Admin",
                         payload: "training",
                       );
                     } else {
@@ -156,7 +157,8 @@ class HomeItemRegistration extends StatelessWidget {
               LocaNotificationServices.showBasicNotification(
                 id: 3,
                 body: S.of(context).registrationClosed,
-                title: S.of(context).admin,
+                // title: S.of(context).admin,
+                title: "Admin",
                 payload: "registration",
               );
 
